897688a6664837c06043175f2529a4b164c064ab
Pham Quoc Thinh <phamquocthinh050501@gmail.com>
2023-07-27 17:43:15 +0700

    new

--- src/pages/KhuyenMai/index.js
+++ src/pages/KhuyenMai/index.js
@@ -43,6 +43,7 @@ const KhuyenMai = () => {
               </p>
             </div>
           </div>
+
           <div className="promotion-block">
             <div className="promotion-block-pic">
               <img
@@ -66,18 +67,14 @@ const KhuyenMai = () => {
                 </strong>
               </p>
               <p>
-                <strong style={{ fontSize: "18px", fontWeight: "normal" }}>
-                  <p>
-                    - Giá vé: 45,000 đ/vé 2D và 55,000 đ/ vé 3D<br></br>- Giảm
-                    10% giá trị hóa đơn bắp nước cho chủ thẻ C’FRIEND và 15% cho
-                    chủ thẻ C’VIP.<br></br>- Chương trình tích điểm thành viên
-                    và các điều kiện thành viên khác được áp dụng. Lưu ý:{" "}
-                    <br></br>- Chỉ áp dụng mua trực tiếp tại quầy.
-                    <br></br> - Chương trình không giới hạn số vé và số lần giao
-                    dịch trong thời gian diễn ra.<br></br> - Không áp dụng cho
-                    các ngày lễ/tết.
-                  </p>
-                </strong>
+                - Giá vé: 45,000 đ/vé 2D và 55,000 đ/ vé 3D<br></br>- Giảm 10%
+                giá trị hóa đơn bắp nước cho chủ thẻ CFRIEND và 15% cho chủ thẻ
+                CVIP.<br></br>- Chương trình tích điểm thành viên và các điều
+                kiện thành viên khác được áp dụng. Lưu ý: <br></br>- Chỉ áp dụng
+                mua trực tiếp tại quầy.
+                <br></br> - Chương trình không giới hạn số vé và số lần giao
+                dịch trong thời gian diễn ra.<br></br> - Không áp dụng cho các
+                ngày lễ/tết.
               </p>
             </div>
           </div>
@@ -106,17 +103,13 @@ const KhuyenMai = () => {
                 </strong>
               </p>
               <p>
-                <strong style={{ fontSize: "18px", fontWeight: "normal" }}>
-                  <p>
-                    Áp dụng dành cho giáo viên, giảng viên, học sinh, sinh viên
-                    và thanh niên dưới 22 tuổi, trên toàn hệ thống. Ưu đãi giá
-                    vé xem phim chỉ 45,000đ/vé 2D.<br></br> LƯU Ý: - Vui lòng
-                    mua TRỰC TIẾP TẠI RẠP và xuất trình thẻ HSSV-GV hoặc CMND có
-                    dán ảnh và còn hiệu lực khi mua vé.<br></br> - Mỗi thẻ mua
-                    được một vé.<br></br> - Không áp dụng cho các ngày Lễ, Tết,
-                    hoặc suất chiếu có phụ thu từ nhà phát hành phim.
-                  </p>
-                </strong>
+                Áp dụng dành cho giáo viên, giảng viên, học sinh, sinh viên và
+                thanh niên dưới 22 tuổi, trên toàn hệ thống. Ưu đãi giá vé xem
+                phim chỉ 45,000đ/vé 2D.<br></br> LƯU Ý: - Vui lòng mua TRỰC TIẾP
+                TẠI RẠP và xuất trình thẻ HSSV-GV hoặc CMND có dán ảnh và còn
+                hiệu lực khi mua vé.<br></br> - Mỗi thẻ mua được một vé.
+                <br></br> - Không áp dụng cho các ngày Lễ, Tết, hoặc suất chiếu
+                có phụ thu từ nhà phát hành phim.
               </p>
             </div>
           </div>
